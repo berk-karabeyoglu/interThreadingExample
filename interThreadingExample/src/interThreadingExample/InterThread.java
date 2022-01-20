7b0ae58cfd14e8f3847bcac5009110b4020e089d
Berk <Berk@LAPTOP-5B61DQVG>
2022-01-20 15:39:54 +0300

    first commit

---
+++ interThreadingExample/src/interThreadingExample/InterThread.java
@@ -0,0 +1,96 @@
+package interThreadingExample;
+
+class A {
+	int num;
+
+	boolean valueSet = false;
+
+	public synchronized void put(int num) {
+		while (valueSet) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Num: " + num);
+		this.num = num;
+		valueSet = true;
+		notify();
+	}
+
+	public synchronized void get() {
+		while (!valueSet) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Get: " + num);
+		valueSet = false;
+		notify();
+	}
+}
+
+class Producer implements Runnable {
+	A a;
+
+	public Producer(A a) {
+		this.a = a;
+		Thread t1 = new Thread(this, "Producer");
+		t1.start();
+	}
+
+	public void run() {
+
+		int i = 0;
+		while (true) {
+			a.put(i++);
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+
+			}
+		}
+	}
+
+}
+
+class Consumer implements Runnable {
+
+	A a;
+
+	public Consumer(A a) {
+		this.a = a;
+		Thread t1 = new Thread(this, "Consumer");
+		t1.start();
+	}
+
+	public void run() {
+
+		int i = 0;
+		while (true) {
+			a.get();
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+
+			}
+		}
+
+	}
+}
+
+public class InterThread {
+
+	public static void main(String[] args) {
+
+		A a = new A();
+		new Producer(a);
+		new Consumer(a);
+
+	}
+}
